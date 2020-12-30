d069c01b51296012206cbd4f91d7b215a250586c
Jarno <jarno>
2020-12-30 10:04:18 +0200

    code

---
+++ TelnetPlugin/TelnetPlugin.cs
@@ -0,0 +1,20 @@
+﻿using VRage.Plugins;
+
+namespace SETelnetPlugin
+{
+    public class TelnetPlugin : IPlugin
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(object gameInstance)
+        {
+            System.IO.File.WriteAllText("c:\\users\\jarno\\documents\\telnetplugin.txt", "Init!\n");
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
